177dafde2de0ab278990f160cd297523ed5db466
MauroErben <mauroerben100@hotmail.com>
2021-12-12 14:56:27 -0300

    se elimino el codigo js

--- js/codigo.js
+++ js/codigo.js
@@ -1,17 +1,1 @@
 
-const nombresProhibidos = ["Franco", "Ingrid", "Thiago", "Mauro"];
-
-const saludar = name => {
-    for(let i of nombresProhibidos){
-        if(name === i || name === i.toLowerCase()){
-            return "Este nombre no esta permitido en nuestro sistema.";
-        }
-    }
-    return `Hola ${name}!`;
-}
-
-document.getElementById("btn").addEventListener("click", () => {
-    const name = document.getElementById("input-name").value;
-    alert(saludar(name));
-    name.value = "";
-});
